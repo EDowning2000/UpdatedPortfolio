2104ece5fd42f25c75c420033d8a2bf6c2501281
EDowning2000 <emersondowning@gmail.com>
2020-09-12 16:13:33 -0400

    added my social photos into the images directory

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import "./App.css"
+import igPic from '../src/Images/instagramPic.jpg'
+import ghPic from '../src/Images/githubPic.png'
+import gmailPic from '../src/Images/gmailPic.jpeg'
+import twitterPic from '../src/Images/twitterPic.jpg'
+import linkedInPic from '../src/Images/LinkedInPic.png'
 
-import './App.css';
 
 function App() {
   return (
@@ -9,11 +14,11 @@ function App() {
         <div className="app_header">Hi, my name is Emerson Downing</div>
           <div className="app_phrase">I am a full-stack developer with a passion for beautiful and highly functional Front-End components, learning new technologies, and best practices</div>
             <div className="app_follow">Follow Me</div>
-              <img src="" className="app_linkedIn"></img>
-              <img src="" className="app_github"></img>
-              <img src="" className="app_twitter"></img>
-              <img src="" className="app_instagram"></img>
-              <img src="" className="app_gmail"></img>
+              <img src={linkedInPic} className="app_linkedIn"></img>
+              <img src={ghPic} className="app_github"></img>
+              <img src={twitterPic} className="app_twitter"></img>
+              <img src={igPic} className="app_instagram"></img>
+              <img src={gmailPic} className="app_gmail"></img>
 
 
 
