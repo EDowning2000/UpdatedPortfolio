63e38055e43c41945639c96a121d10c3a3f9d7ff
EDowning2000 <emersondowning@gmail.com>
2020-09-12 15:53:21 -0400

    added most of the pieces for the front page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="app_main">
+        <div className="app_header">Hi, my name is Emerson Downing</div>
+          <div className="app_phrase">I am a full-stack developer with a passion for beautiful and highly functional Front-End components, learning new technologies, and best practices</div>
+            <div className="app_follow">Follow Me</div>
+              <img src="" className="app_linkedIn"></img>
+              <img src="" className="app_github"></img>
+              <img src="" className="app_twitter"></img>
+              <img src="" className="app_instagram"></img>
+              <img src="" className="app_gmail"></img>
+
+
+
+
+
+
+
+
+
+      </div>
       
     </div>
   );
