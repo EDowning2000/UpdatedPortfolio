ee14da54f6721257ff7c1999d651eebdc6554e59
EDowning2000 <emersondowning@gmail.com>
2020-09-12 16:25:17 -0400

    got the images alignedgit add -A! but still WIP cause i gotta get off

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,30 @@
-import React from 'react';
-import "./App.css"
-import igPic from '../src/Images/instagramPic.jpg'
-import ghPic from '../src/Images/githubPic.png'
-import gmailPic from '../src/Images/gmailPic.jpeg'
-import twitterPic from '../src/Images/twitterPic.jpg'
-import linkedInPic from '../src/Images/LinkedInPic.png'
-
+import React from "react";
+import "./App.css";
+import igPic from "../src/Images/instagramPic.jpg";
+import ghPic from "../src/Images/githubPic.png";
+import gmailPic from "../src/Images/gmailPic.jpeg";
+import twitterPic from "../src/Images/twitterPic.jpg";
+import linkedInPic from "../src/Images/LinkedInPic.png";
 
 function App() {
   return (
     <div className="App">
       <div className="app_main">
         <div className="app_header">Hi, my name is Emerson Downing</div>
-          <div className="app_phrase">I am a full-stack developer with a passion for beautiful and highly functional Front-End components, learning new technologies, and best practices</div>
-            <div className="app_follow">Follow Me</div>
-              <img src={linkedInPic} className="app_linkedIn"></img>
-              <img src={ghPic} className="app_github"></img>
-              <img src={twitterPic} className="app_twitter"></img>
-              <img src={igPic} className="app_instagram"></img>
-              <img src={gmailPic} className="app_gmail"></img>
-
-
-
-
-
-
-
-
-
+        <div className="app_phrase">
+          I am a full-stack developer with a passion for beautiful and highly
+          functional Front-End components, learning new technologies, and best
+          practices
+        </div>
+        <div className="app_follow">Follow Me</div>
+        <div className="app_socialContainer">
+          <img src={linkedInPic} className="app_linkedIn"></img>
+          <img src={ghPic} className="app_github"></img>
+          <img src={twitterPic} className="app_twitter"></img>
+          <img src={igPic} className="app_instagram"></img>
+          <img src={gmailPic} className="app_gmail"></img>
+        </div>
       </div>
-      
     </div>
   );
 }
