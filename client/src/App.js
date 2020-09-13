95e839ca9b0ca9821b4327d85ef5501e3523ee34
EDowning2000 <emersondowning@gmail.com>
2020-09-12 23:18:14 -0400

    got the flex containers and all the content for the homepage finished along with most of the styling

--- client/src/App.js
+++ client/src/App.js
@@ -9,21 +9,29 @@ import linkedInPic from "../src/Images/LinkedInPic.png";
 function App() {
   return (
     <div className="App">
-      <div className="app_main">
-        <div className="app_header">Hi, my name is Emerson Downing</div>
-        <div className="app_phrase">
+      <div className="app_socialContainer">
+        <div className="app_follow">Follow Me</div>
+        <img
+          src={linkedInPic}
+          className="app_linkedIn"
+          alt="LinkedIn Logo"
+        ></img>
+        <img src={ghPic} className="app_github" alt="Github Logo"></img>
+        <img src={twitterPic} className="app_twitter" alt="Twitter Logo"></img>
+        <img src={igPic} className="app_instagram" alt="Instagram Logo"></img>
+        <img src={gmailPic} className="app_gmail" alt="Gmail Logo"></img>
+      </div>
+
+      <div className="app_me">
+        <div className="app_header">Hi, my name is Emerson Downing!</div>
+        <div className="app_motto">
           I am a full-stack developer with a passion for beautiful and highly
           functional Front-End components, learning new technologies, and best
           practices
         </div>
-        <div className="app_follow">Follow Me</div>
-        <div className="app_socialContainer">
-          <img src={linkedInPic} className="app_linkedIn"></img>
-          <img src={ghPic} className="app_github"></img>
-          <img src={twitterPic} className="app_twitter"></img>
-          <img src={igPic} className="app_instagram"></img>
-          <img src={gmailPic} className="app_gmail"></img>
-        </div>
+      </div>
+      <div className="app_right">
+        im a placeholder
       </div>
     </div>
   );
