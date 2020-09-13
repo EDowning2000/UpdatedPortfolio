1dfd5680990e6855f04677cfb163a70ee414bad0
EDowning2000 <emersondowning@gmail.com>
2020-09-13 13:10:01 -0400

    added the links to my social accounts, also added a copy button to my gmail acct now im gonna get working on the hamburger button

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,45 @@ import gmailPic from "../src/Images/gmailPic.jpeg";
 import twitterPic from "../src/Images/twitterPic.jpg";
 import linkedInPic from "../src/Images/LinkedInPic.png";
 
+import Clipboard from "react-clipboard.js";
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
+
+
 function App() {
   return (
     <div className="App">
       <div className="app_socialContainer">
         <div className="app_follow">Follow Me</div>
-        <img
-          src={linkedInPic}
-          className="app_linkedIn"
-          alt="LinkedIn Logo"
-        ></img>
-        <img src={ghPic} className="app_github" alt="Github Logo"></img>
-        <img src={twitterPic} className="app_twitter" alt="Twitter Logo"></img>
-        <img src={igPic} className="app_instagram" alt="Instagram Logo"></img>
-        <img src={gmailPic} className="app_gmail" alt="Gmail Logo"></img>
+        <a
+          href="https://www.linkedin.com/in/emerson-downing-baaa43195/"
+          target="blank"
+        >
+          <img
+            src={linkedInPic}
+            className="app_linkedIn"
+            alt="LinkedIn Logo"
+          ></img>
+        </a>
+        <a href="https://github.com/EDowning2000" target="blank">
+          <img src={ghPic} className="app_github" alt="Github Logo"></img>
+        </a>
+        <a href="https://twitter.com/EmersonDowning1" target="blank">
+          <img
+            src={twitterPic}
+            className="app_twitter"
+            alt="Twitter Logo"
+          ></img>
+        </a>
+        <a
+          href="https://www.instagram.com/emersondowning__/?hl=en"
+          target="blank"
+        >
+          <img src={igPic} className="app_instagram" alt="Instagram Logo"></img>
+        </a>
+        <Clipboard data-clipboard-text="emersondowning@gmail.com">
+          <img src={gmailPic} className="app_gmail" alt="Gmail Logo"></img>
+        </Clipboard>
       </div>
 
       <div className="app_me">
@@ -31,7 +56,9 @@ function App() {
         </div>
       </div>
       <div className="app_right">
-        im a placeholder
+        <div className="app_menu">
+          <div className="app_burger"></div>
+        </div>
       </div>
     </div>
   );
