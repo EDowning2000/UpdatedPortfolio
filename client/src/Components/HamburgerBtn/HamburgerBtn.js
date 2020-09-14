12293a82ca512d2298d657dedfdaa54ea665d740
EDowning2000 <emersondowning@gmail.com>
2020-09-13 21:46:42 -0400

    honestly quite the challenge figuring out how to render a dynamic className into the first div but its up and running

--- client/src/Components/HamburgerBtn/HamburgerBtn.js
+++ client/src/Components/HamburgerBtn/HamburgerBtn.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./HamburgerBtn.css";
 
 function HamburgerBtn() {
+  const [openMenu, setOpenMenu] = useState("false");
+  const [open, setOpen] = useState("menu-btn");
+
+  const openSesame = () => {
+    if (openMenu === "false") {
+      setOpenMenu("true")
+      setOpen("menu-btn open")
+    } else {
+      setOpenMenu("false")
+      setOpen("menu-btn")
+    }
+  };
+
   return (
-    <div class="menu-btn">
-      <div class="menu-btn__burger"></div>
+    <div className={open} onClick={openSesame}>
+      <div className="menu-btn__burger"></div>
     </div>
   );
 }
