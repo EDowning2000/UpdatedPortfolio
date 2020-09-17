cd3cb05ec14bd4d4654d2179bc11b03b2340f22b
EDowning2000 <emersondowning@gmail.com>
2020-09-17 16:41:23 -0400

    this isnt really working

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import igPic from "../src/Images/instagramPic.jpg";
 import ghPic from "../src/Images/githubPic.png";
 import gmailPic from "../src/Images/gmailPic.jpeg";
@@ -13,7 +13,7 @@ import Clipboard from "react-clipboard.js";
 import Tippy from "@tippyjs/react";
 import "tippy.js/dist/tippy.css";
 import anime from "animejs/lib/anime.es.js";
- 
+
 //how to do  routes template
 //<Router>
 //  <Switch>
@@ -78,80 +78,73 @@ function App() {
   return (
     <div className="App">
       <Router>
-      <div className="app_topContainer" id="app_topContainer">
-        <div className="app_follow">Follow Me</div>
-        <a
-          href="https://www.linkedin.com/in/emerson-downing-baaa43195/"
-          target="blank"
-        >
-          <img
-            src={linkedInPic}
-            className="app_linkedIn socialLink"
-            alt="LinkedIn Logo"
-          ></img>
-        </a>
-        <a href="https://github.com/EDowning2000" target="blank">
-          <img
-            src={ghPic}
-            className="app_github socialLink"
-            alt="Github Logo"
-          ></img>
-        </a>
-        <a href="https://twitter.com/EmersonDowning1" target="blank">
-          <img
-            src={twitterPic}
-            className="app_twitter socialLink"
-            alt="Twitter Logo"
-          ></img>
-        </a>
-        <a
-          href="https://www.instagram.com/emersondowning__/?hl=en"
-          target="blank"
-        >
-          <img
-            src={igPic}
-            className="app_instagram socialLink"
-            alt="Instagram Logo"
-          ></img>
-        </a>
-        <Clipboard data-clipboard-text="emersondowning@gmail.com">
-          <img
-            src={gmailPic}
-            className="app_gmail socialLink"
-            alt="Gmail Logo"
-          ></img>
-        </Clipboard>
-      </div>
+        <div className="app_topContainer" id="app_topContainer">
+          <div className="app_follow">Follow Me</div>
+          <a
+            href="https://www.linkedin.com/in/emerson-downing-baaa43195/"
+            target="blank"
+          >
+            <img
+              src={linkedInPic}
+              className="app_linkedIn socialLink"
+              alt="LinkedIn Logo"
+            ></img>
+          </a>
+          <a href="https://github.com/EDowning2000" target="blank">
+            <img
+              src={ghPic}
+              className="app_github socialLink"
+              alt="Github Logo"
+            ></img>
+          </a>
+          <a href="https://twitter.com/EmersonDowning1" target="blank">
+            <img
+              src={twitterPic}
+              className="app_twitter socialLink"
+              alt="Twitter Logo"
+            ></img>
+          </a>
+          <a
+            href="https://www.instagram.com/emersondowning__/?hl=en"
+            target="blank"
+          >
+            <img
+              src={igPic}
+              className="app_instagram socialLink"
+              alt="Instagram Logo"
+            ></img>
+          </a>
+          <Clipboard data-clipboard-text="emersondowning@gmail.com">
+            <img
+              src={gmailPic}
+              className="app_gmail socialLink"
+              alt="Gmail Logo"
+            ></img>
+          </Clipboard>
+        </div>
 
-      <div className="app_me">
-        {/* <div className={show}> */}
+        <div className="app_me">
+          <div className={show}>
 
-      {/* <Router> */}
-        <Switch>
-          <Route path="/" exact component={AboutMe}/>
-          <Route path ='/links' component={Links}/>
-          {/* to be finished later*/}
-          {/* <Route path="/projects" exact component={MyProjects}/>
-          <Route path="/technologies" exact component={MtTechnologies}/> */}
-        </Switch>
-      {/* </Router> */}
+          <Switch>
+            <Route path="/" exact component={AboutMe} />
+          <div className={hide}>
+          <Links />
+        </div> 
+          </Switch>
 
           {/* <AboutMe /> */}
-        {/* </div> */}
+          </div>
 
-        {/* <div className={hide}>
-          <Links />
-        </div> */}
-      </div>
-      <div className="app_right">
-        
-        <Link to='/links'>
-        <div className={open} onClick={openSesame}>
-          <div className="menu-btn__burger"></div>
+           
         </div>
-        </Link>
-        
-      </div>
+        <div className="app_right">
+          <Link to="/links">
+            <div className={open} onClick={openSesame}>
+              <div className="menu-btn__burger"></div>
+            </div>
+          </Link>
+        </div>
       </Router>
     </div>
   );
