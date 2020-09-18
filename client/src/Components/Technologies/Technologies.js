29ff5984307d85efd52e6cfb2bac1e42c966ebbd
EDowning2000 <emersondowning@gmail.com>
2020-09-17 20:19:16 -0400

    added the scaffolding into the technologies page now i just need the images and styling

--- client/src/Components/Technologies/Technologies.js
+++ client/src/Components/Technologies/Technologies.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import "./Technologies.css";
 
 function Technologies() {
   return (
-    <div>
-      random filler text to see if this baby is working 
+    <div className="technologies">
+      <div className="myTech">My Technologies</div>
+      <div className="htmlTech">
+        <img className="" src={} alt=""></img>
+        <img className="" src={} alt=""></img>
+      </div>
+      <div className="stylingTech">
+        <img className="" src={} alt=""></img>
+        <img className="" src={} alt=""></img>
+        <img className="" src={} alt=""></img>
+        <img className="" src={} alt=""></img>
+      </div>
+      <div className="jsTech">
+        <img className="" src={} alt=""></img>
+        <img className="" src={} alt=""></img>
+        <img className="" src={} alt=""></img>
+      </div>
+      <div className="backendTech">
+        <img className="" src={} alt=""></img>
+        <img className="" src={} alt=""></img>
+      </div>
+      <div className="databaseTech">
+        <img className="" src={} alt=""></img>
+        <img className="" src={} alt=""></img>
+        <img className="" src={} alt=""></img>
+      </div>
+      <div className="designTech">
+        <img className="" src={} alt=""></img>
+        <img className="" src={} alt=""></img>
+      </div>
+      <div className="deploymentTech">
+        <img className="" src={} alt=""></img>
+        <img className="" src={} alt=""></img>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Technologies
+export default Technologies;
