9450ad6382fc3e9e15f6d7e25b02fa898dff5698
EDowning2000 <emersondowning@gmail.com>
2020-10-02 16:13:16 -0400

    changed some of the fliters to make them look a bit more professional and less amateur in my opinion:

--- client/src/Components/ProjectsTemplate/ProjectsHome/HomeTemplate.js
+++ client/src/Components/ProjectsTemplate/ProjectsHome/HomeTemplate.js
@@ -4,6 +4,7 @@ import './HomeTemplate.css'
 function HomeTemplate({homepagePic, title}) {
   return (
     <div className="container">
+      {/* <div className="header"><span className="first"></span><span className="second"></span><span className="third"></span></div> */}
       <img src={homepagePic}/>
       <h3 className='title'>{title}</h3>
     </div>
