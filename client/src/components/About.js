16b74946cb78d21bc586748751be6eb4b67629dc
EDowning2000 <emersondowning@gmail.com>
2020-10-27 12:59:36 -0400

    got all my images added in for the cards

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import './About.css'
+import frontEndPic from '../images/frontEndPic.png'
+import backEndPic from '../images/backEndPic.png'
+import studentPic from '../images/studentPic.png'
 
 function About() {
   return (
@@ -12,7 +15,17 @@ function About() {
         Since beginning my coding journey in late 2018, I have honed my skills as not only a good developer, but also an attractive personality for the job. I have worked remotely with partners on large projects, collaborated with multiple small groups towards a goal, and gained lots of experience in terms of finding answers to the problems at hand in just my few years of experience. I am focused, detail and improvement oriented, and always eager to learn and apply my skills whereever helpful!
       </div>
 
-
+      <div className="aboutCards">
+        <div className="cardFrontEnd">
+          <img src={frontEndPic} className="frontEndPic"/>
+        </div>
+        <div className="cardBackEnd">
+          <img src={backEndPic}/>
+        </div>
+        <div className="cardStudent">
+          <img src={studentPic}/>
+        </div>
+      </div>
 
 
 
