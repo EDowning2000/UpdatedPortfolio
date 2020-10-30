c153ac45377ec6e7690a75fcdfb32c6f924e63c6
EDowning2000 <emersondowning@gmail.com>
2020-10-30 15:45:15 -0400

    got the single project portion all finished

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,29 +1,36 @@
-import React from 'react'
-import './Projects.css'
+import React from "react"
+import "./Projects.css"
 
-import SingleProject from './singleProject'
+import SingleProject from "./singleProject"
 
-import blog1 from '../images/blogHome.png'
-import blog2 from '../images/blogHome2.png'
-import books from '../images/booksHome.png'
-import books2 from '../images/booksHome2.png'
-import budget from '../images/budgetHome.png'
-import budget2 from '../images/budgetHome2.png'
-import burger from '../images/burgerHome.png'
-import burger2 from '../images/burgerHome2.png'
-import covid from '../images/covidHome.png'
-import covid2 from '../images/covidHome2.png'
+import blog1 from "../images/blogHome.png"
+import blog2 from "../images/blogHome2.png"
+import books from "../images/booksHome.png"
+import books2 from "../images/booksHome2.png"
+import budget from "../images/budgetHome.png"
+import budget2 from "../images/budgetHome2.png"
+import burger from "../images/burgerHome.png"
+import burger2 from "../images/burgerHome2.png"
+import covid from "../images/covidHome.png"
+import covid2 from "../images/covidHome2.png"
 
 function Projects() {
   return (
     <div className="projectsContainer">
       <div className="projectsHeader">My Recent Works</div>
-      <div className="projectsHeader2">I feel these works are an accurate description of what I am capable of. <br/>Note: I am <span className='projectsColor'>not</span> a UI/UX designer</div>
-
-
-
-
-
+      <div className="projectsHeader2">
+        I feel these works are an accurate description of what I am capable of.{" "}
+        <br />
+        Note: I am <span className="projectsColor">not</span> a UI/UX designer
+      </div>
+      <div className="singleProjectContainer"></div>
+      <SingleProject
+        image1={blog1}
+        title="Blog App"
+        description="this applicaton is an application that allows you to create, read, delete blog posts through your own website. Its pretty nifty i must say."
+        tech="HTML, css, javascript, react"
+        repo="asdlfkjlasjdf"
+      />
     </div>
   )
 }
--- client/src/components/singleProject.js
+++ client/src/components/singleProject.js
@@ -1,15 +1,33 @@
 import React from "react"
 import "./singleProject.css"
+import ghPic from "../images/githubPic2.png"
 
-function singleProject({image1, description, title, tech, repo}) {
+import { TwitterShareButton, TwitterTweetEmbed } from "react-twitter-embed"
+
+function singleProject({ image1, description, title, tech, repo }) {
   return (
     <div className="singleMainContainer">
       <img src={image1} className="singleImage" />
+      <hr/>
       <div className="singleContainer">
         <div className="singleTitle">{title}</div>
         <div className="singleDescription">{description}</div>
         <div className="singleTech">{tech}</div>
-        <div className="singleRepo">{repo}</div>
+        <div className="repoInfo">
+          <div className="twitterPic">
+          <TwitterShareButton
+          
+            url={repo}
+            options={{
+              text:
+                "This project from Emerson is pretty good, i can find it here =>",
+              via: "EmersonDowning1",
+            }}
+          /></div>
+          <a href={repo} target="blank">
+            <img src={ghPic} className="repoPic" />
+          </a>
+        </div>
       </div>
     </div>
   )
