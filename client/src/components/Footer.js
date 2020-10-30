01fa466c5cfbb0383fd96e4a6f7f503842062cd4
EDowning2000 <emersondowning@gmail.com>
2020-10-30 19:56:48 -0400

    everything is finished. just need to get my projects in there as well as work on the responsiveness

---
+++ client/src/components/Footer.js
@@ -0,0 +1,36 @@
+import React from "react"
+import "./Footer.css"
+import mainLogo from "../images/emerLogo.png"
+
+function Footer() {
+  return (
+    <div className="footerContainer">
+      <img className="footerLogo" src={mainLogo} />
+      <div className="footerMotto">Making lives easier with software.</div>
+
+      <span className="footerFind">Find me here!</span>
+      <div className="linkContainer">
+        <a href="https://twitter.com/EmersonDowning1" target="blank">
+          <div className="fab fa-twitter fa-2x"></div>
+        </a>
+        <a href="https://github.com/EDowning2000" target="blank">
+          <div className="fab fa-github fa-2x"></div>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/emerson-downing-baaa43195/"
+          target="blank"
+        >
+          <div className="fab fa-linkedin-in fa-2x"></div>
+        </a>
+
+        <a href="https://www.instagram.com/emersondowning_/" target="blank">
+          <div className="fab fa-instagram fa-2x"></div>
+        </a>
+      </div>
+
+      <div className="footerMade">Made with love by Emerson Downing</div>
+    </div>
+  )
+}
+
+export default Footer
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,6 +4,7 @@ import Header from "../components/Header"
 import Intro from "../components/Intro"
 import About from "../components/About"
 import Projects from '../components/Projects'
+import Footer from '../components/Footer'
 
 const IndexPage = () => (
   <div className="main">
@@ -11,6 +12,7 @@ const IndexPage = () => (
     <Intro />
     <About/>
     <Projects/>
+    <Footer/>
   </div>
 )
 
