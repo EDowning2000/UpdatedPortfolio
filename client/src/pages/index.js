efcced0384b8a898948b01c8f25a48ee4760286b
EDowning2000 <emersondowning@gmail.com>
2020-11-09 13:41:27 -0500

    got all the media queries done for the header

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,20 +3,20 @@ import "./index.css"
 import Header from "../components/Header"
 import Intro from "../components/Intro"
 import About from "../components/About"
-import Projects from '../components/Projects'
-import Footer from '../components/Footer'
-import { Helmet } from 'react-helmet'
+import Projects from "../components/Projects"
+import Footer from "../components/Footer"
+import { Helmet } from "react-helmet"
 import mainLogo from "../images/emerLogo.png"
 
 const IndexPage = () => (
   <div className="main">
-
-<Helmet>
-<script src="https://kit.fontawesome.com/e47e81ddf8.js" crossorigin="anonymous"></script>
-
-    <title>Emerson Downing</title>
-
-</Helmet>
+    <Helmet>
+      <script
+        src="https://kit.fontawesome.com/e47e81ddf8.js"
+        crossorigin="anonymous"
+      ></script>
+      <title>Emerson Downing</title>
+    </Helmet>
 
     <Header />
     {/* <Intro />
